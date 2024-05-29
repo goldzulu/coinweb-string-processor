7b26d39a045d1e91720f0bcfcbef7e7269a13216
dimisus <dimisus@coinweb.io>
2024-05-29 12:26:41 +0200

    fix: Update yarn.lock + vite.config

--- packages/with-react/vite.config.ts
+++ packages/with-react/vite.config.ts
@@ -1,5 +1,4 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
